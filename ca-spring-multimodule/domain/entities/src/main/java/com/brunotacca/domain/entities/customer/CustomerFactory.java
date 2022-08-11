9a364534377e0db89013ef8e6830fe2975bab517
Bruno Tacca <brunotacca@gmail.com>
2022-08-10 21:51:40 -0300

    factory method to reconstitute existing

--- ca-spring-multimodule/domain/entities/src/main/java/com/brunotacca/domain/entities/customer/CustomerFactory.java
+++ ca-spring-multimodule/domain/entities/src/main/java/com/brunotacca/domain/entities/customer/CustomerFactory.java
@@ -3,6 +3,7 @@ package com.brunotacca.domain.entities.customer;
 import java.util.UUID;
 
 import com.brunotacca.domain.entities.shared.exceptions.BusinessException;
+import com.brunotacca.domain.entities.shared.exceptions.causes.RequiredFieldException;
 
 public class CustomerFactory {
 
@@ -10,10 +11,26 @@ public class CustomerFactory {
     final Customer customer = new DefaultCustomer(UUID.randomUUID().toString(), name, email, address);
     return customer;
   }
-  
+
+  public Customer getExistingCustomer(String id, String name, String email, Address address) throws BusinessException {
+    UUID uuid = null;
+    if (id == null || id.trim().isEmpty()) {
+      throw new RequiredFieldException("id");
+    }
+
+    try {
+      uuid = UUID.fromString(id);
+    } catch (IllegalArgumentException e) {
+      throw new RequiredFieldException("id");
+    }
+
+    final Customer customer = new DefaultCustomer(uuid.toString(), name, email, address);
+    return customer;
+  }
+
   public Address createAddress(String street, String number, String zip, String city) throws BusinessException {
     final Address address = new Address(street, number, zip, city);
     return address;
   }
-  
+
 }
